de6d503a872d0920448b50a2320e3bfe56666501
rcrampton1 <rcrampton1@users.noreply.github.com>
2022-02-28 07:57:33 +0000

    Terraform State Create

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket = "servian-terraform-state"
+    bucket = "servian-terraform-state-65823141"
     key    = "terraform.tfstate"
-    region = "eu-west-1"
+    region = "eu-west-2"
 
-    dynamodb_table = "servian-terraform-lock"
+    dynamodb_table = "servian-terraform-lock-65823141"
     encrypt        = true
   }
 }
