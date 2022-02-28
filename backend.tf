218e7604fb399ba54b439ad04b1b0f62f9f0fcf1
rcrampton1 <rich@richanddan.com>
2022-02-28 11:11:55 +0000

    correct format for backend

--- backend.tf
+++ backend.tf
@@ -6,9 +6,9 @@ terraform {
   backend "s3" {
     bucket = "servian-terraform-state" #hard coded look to replace
     key    = "terraform.tfstate"
-    region = "eu-west-1"  #hard coded look to replace
+    region = "eu-west-1" #hard coded look to replace
 
-    dynamodb_table = "servian-terraform-lock"  #hard coded look to replace
+    dynamodb_table = "servian-terraform-lock" #hard coded look to replace
     encrypt        = true
   }
 }
